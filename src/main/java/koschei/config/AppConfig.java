d42db42a26c5d615fc9fbc7db17950007ba42bcc
borisat <118672475+borisat@users.noreply.github.com>
2023-05-03 19:10:27 +0300

    commit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -2,6 +2,7 @@ package koschei.config;
 
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+    @Bean(name = "island2")
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
